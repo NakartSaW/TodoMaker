dbce2310b140c595a9d4dab3b73444b3c08320ed
NakartSaW <ggl.clvr@gmail.com>
2025-02-23 17:21:31 +0100

    Init project

---
+++ src/main/java/org/example/TodoMakerApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoMakerApplication.class, args);
+    }
+
+}
